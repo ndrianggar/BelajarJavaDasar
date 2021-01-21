74a210772caa913892c71d4e83d20adcd8653620
Hendri <ndrianggar@gmail.com>
2021-01-21 14:37:15 +0700

    Tambah Capture 32

---
+++ Java [Dasar] - 32 - Break, Continue, dan Return (Control Flow)/src/com/BelajarJava/Main.java
@@ -0,0 +1,23 @@
+package com.BelajarJava;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Break, Continue, dan Return
+
+        int a = 0;
+
+        while (true){
+            a++;
+
+            if(a == 10){
+                break;
+            }else if(a == 5){
+                continue;
+            }else if(a == 7){
+                return;
+            }
+            System.out.println("Looping ke - " + a);
+        }
+    }
+}
